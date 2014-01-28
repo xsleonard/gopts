054e9bce760ba34ef07d5854a3d2989366e16f6c
Steve Leonard <xsleonard@gmail.com>
2014-01-28 17:49:19 -0500

    supporting returning previous Option; add examples

--- gopts.go
+++ gopts.go
@@ -1,4 +1,55 @@
-// Helpers for struct configuration
+/* Helpers for struct configuration.
+
+Use it like so:
+
+    import "gopts"
+
+    var (
+        Port = gopts.Opt("Config.Port", uint16(7777))
+        Network = gopts.Opt("Network", "tcp")
+        // ...
+    )
+
+    type ServerConfig struct {
+        Port uint16
+        // ...
+    }
+
+    type Server struct {
+        Network string
+        Config ServerConfig
+        // ...
+    }
+
+    func NewServer(opts ...gopts.OptSetter) *Server {
+        s := &Server{}
+        // set defaults
+        gopts.SetOptions(Port(), Network())
+        // override defaults with user's values
+        gopts.SetOptions(opts...)
+        return s
+    }
+
+    func updateServer() {
+        s := NewServer(Network("udp"))
+        // s.Config.Port == 7777
+        // s.Network == "udp"
+
+        // Set multiple options
+        prevOpts := gopts.SetOptions(s, Port(uint16(6666)), Network("tcp"))
+        // s.Config.Port == 6666
+        // s.Network == "tcp"
+
+        // Sets a single option
+        prevPort := gopts.SetOption(s, prevOpts[0])
+        // s.Config.Port == 7777, back to default
+
+        // Reset to the previous value. These two statements are equivalent.
+        gopts.SetOption(s, prevPort)
+        prevPort(s)
+        // s.Config.Port == 6666
+    }
+*/
 package gopts
 
 import (
@@ -13,13 +64,13 @@ type Opt func(...interface{}) OptSetter
 
 // Sets a field's value on a wrapped struct.  interface{} must be an
 // addressable type, i.e. a pointer to a struct.
-type OptSetter func(interface{})
+type OptSetter func(interface{}) OptSetter
 
 // Creates an Opt for field name and default value defVal.  Name must be
 // capitalized since only public fields can be set with reflection.
-// Nested field names are supported.  Nested fields
+// Nested field names are supported with "A.B.C" syntax.
 func Option(name string, defVal interface{}) Opt {
-    // Pre-verify that all names will be exportable
+    // Verify that all names will be exportable
     if len(name) == 0 {
         log.Panic("Option name cannot be empty")
     }
@@ -30,7 +81,10 @@ func Option(name string, defVal interface{}) Opt {
             log.Panicf("%s is not an exported field name", name)
         }
     }
+    return newOpt(name, defVal)
+}
 
+func newOpt(name string, defVal interface{}) Opt {
     return func(_val ...interface{}) OptSetter {
         // Unpack the arguments or use the default value
         var val reflect.Value
@@ -41,33 +95,47 @@ func Option(name string, defVal interface{}) Opt {
         } else {
             val = reflect.ValueOf(_val[0])
         }
-
-        return func(self interface{}) {
-            obj := reflect.ValueOf(self)
-            for _, f := range fields[:len(fields)-1] {
-                obj = reflect.Indirect(obj).FieldByName(f)
-                if !obj.IsValid() {
-                    log.Panicf("Unknow field %s", f)
-                }
-            }
-            // Use reflection to set the name on the self struct
-            // self must be a pointer to a struct
-            name := fields[len(fields)-1]
-            field := reflect.Indirect(obj).FieldByName(name)
-            if !field.IsValid() {
-                log.Panicf("Unknown field %s", name)
-            }
-            if !field.CanSet() {
-                log.Panicf("Can't set field on Option target, use a pointer")
-            }
-            field.Set(val)
-        }
+        return newOptSetter(name, val)
     }
 }
 
-// Sets multiple Options on a struct pointer
-func SetOptions(obj interface{}, opts ...OptSetter) {
-    for _, o := range opts {
-        o(obj)
+func newOptSetter(name string, val reflect.Value) OptSetter {
+    fields := strings.Split(name, ".")
+    return func(self interface{}) OptSetter {
+        obj := reflect.ValueOf(self)
+        for _, f := range fields[:len(fields)-1] {
+            obj = reflect.Indirect(obj).FieldByName(f)
+            if !obj.IsValid() {
+                log.Panicf("Unknow field %s", f)
+            }
+        }
+        // Use reflection to set the name on the self struct
+        // self must be a pointer to a struct
+        fieldName := fields[len(fields)-1]
+        field := reflect.Indirect(obj).FieldByName(fieldName)
+        if !field.IsValid() {
+            log.Panicf("Unknown field %s", name)
+        }
+        if !field.CanSet() {
+            log.Panicf("Can't set field on Option target, use a pointer")
+        }
+        prev := reflect.ValueOf(field.Interface())
+        field.Set(val)
+        return newOptSetter(name, prev)
     }
+}
+
+// Sets multiple Options on a struct pointer.  Returns the previous Options'
+// values in order they were passed to this function.
+func SetOptions(obj interface{}, opts ...OptSetter) []OptSetter {
+    prevs := make([]OptSetter, 0, len(opts))
+    for _, o := range opts {
+        prevs = append(prevs, o(obj))
+    }
+    return prevs
+}
+
+// Sets a single Option on a struct pointer.
+func SetOption(obj interface{}, opt OptSetter) OptSetter {
+    return opt(obj)
 }
--- gopts_test.go
+++ gopts_test.go
@@ -1,6 +1,9 @@
 package gopts
 
-import "fmt"
+import (
+    "fmt"
+    "testing"
+)
 
 func ExampleOptions() {
     port := Option("Port", uint16(7777))
@@ -52,4 +55,82 @@ func ExampleNestedOptionsWithPointer() {
     // 8888
     // 7777
     // 6666
+}
+
+var (
+    Port    = Option("Config.Port", uint16(7777))
+    Network = Option("Network", "tcp")
+    // ...
+)
+
+type ServerConfig struct {
+    Port uint16
+    // ...
+}
+
+type Server struct {
+    Network string
+    Config  ServerConfig
+    // ...
+}
+
+func NewServer(opts ...OptSetter) *Server {
+    s := &Server{}
+    // set defaults
+    SetOptions(s, Port(), Network())
+    // override defaults with user's values
+    SetOptions(s, opts...)
+    return s
+}
+
+func TestSetOptions(t *testing.T) {
+    s := NewServer(Network("udp"))
+    // s.Config.Port == 7777
+    // s.Network == "udp"
+    if s.Config.Port != 7777 {
+        t.Fail()
+    }
+    if s.Network != "udp" {
+        t.Fail()
+    }
+
+    // Set multiple options
+    prevOpts := SetOptions(s, Port(uint16(6666)), Network("tcp"))
+    // s.Config.Port == 6666
+    // s.Network == "tcp"
+
+    if s.Config.Port != 6666 {
+        t.Fail()
+    }
+    if s.Network != "tcp" {
+        t.Fail()
+    }
+    if len(prevOpts) != 2 {
+        t.Fail()
+    }
+
+    // Sets a single option
+    prevPort := SetOption(s, prevOpts[0])
+    // s.Config.Port == 7777, back to default
+    if s.Config.Port != 7777 {
+        t.Fail()
+    }
+
+    // Reset to the previous value. These two statements are equivalent.
+    SetOption(s, prevPort)
+    // s.Config.Port == 6666
+    if s.Config.Port != 6666 {
+        t.Fail()
+    }
+    prevPort(s)
+    // s.Config.Port == 6666
+    if s.Config.Port != 6666 {
+        t.Fail()
+    }
+
+    // Another example of avoid SetOption
+    Network("udp")(s)
+    if s.Network != "udp" {
+        t.Fail()
+    }
 }
